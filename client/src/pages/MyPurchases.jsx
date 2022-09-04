1253a2593ab4c867a298dfd0dce4ecaa4f8038f7
07_Itamar_Melamed <nadav8.tch07@gmail.com>
2022-09-04 14:27:17 +0300

    progress purchases page

--- client/src/pages/MyPurchases.jsx
+++ client/src/pages/MyPurchases.jsx
@@ -21,38 +21,66 @@ export default class MyPurchases extends Component {
     render() {
         const { cartId, lineItems } = this.state;
         return (
+            <div className="container-fluid row">
+            <div className="col-3 row">
             <div
             className="card"
           >
             <div className="card-body">
-              <h1 className="card-title">Cart:</h1>
-            </div>
+              <h1 style={{fontSize: "100px"}} className="card-title">Total:</h1>
+            
             <ul className="list-group list-group-flush">
-                {lineItems.map((cartItem) => 
-                <div className="list-group-item">
-                    <img alt="..." src={getById(cartItem.productId).imageUrl} style={{ width: "100px", height: "100px", objectFit: "cover" }}/>
-                    <h3>{getById(cartItem.productId).product_name}</h3>
-                    <h4 style={{color: "orange"}}>${getById(cartItem.productId).price}</h4>
-                    <h5 style={{color: "blue"}}>x{cartItem.quantity}</h5>
-                </div>
-                )}
+                <h1 style={{fontSize: "70px", color: "orange"}}>${getTotalPrice(lineItems)}</h1>
             </ul>
-            <Link to={`/checkout`}>
+            <p/>
+            <Link to={`/`}>
             <button
-                type="button"
-                className="btn btn-success"
-              >
-                Checkout
-              </button>
-              </Link>
-              <Link to={`/`}>
-            <button
+                style={{fontSize: "25px"}}
                 type="button"
                 className="btn btn-secondary"
               >
                 Continue Shopping
               </button>
               </Link>
+              <p/>
+              <Link to={`/checkout`}>
+            <button
+                style={{fontSize: "25px"}}
+                type="button"
+                className="btn btn-success"
+              >
+                Checkout
+              </button>
+              </Link>
+              </div>
+            </div>
+            </div>
+            <div className="col-9 row">
+            <div
+            className="card"
+          >
+            <div className="card-body">
+              <h1 className="card-title">Cart:</h1>
+            
+            <ul className="list-group list-group-flush">
+                <div className="d-flex" style={{justifyContent: "space-evenly"}}>
+                {lineItems.map((cartItem) => 
+                    <div>
+                        <img alt="..." src={getById(cartItem.productId).imageUrl} style={{ width: "100px", height: "100px", objectFit: "cover" }}/>
+                        <h4>{getById(cartItem.productId).product_name}</h4>
+                        <h4 style={{color: "orange"}}>${getById(cartItem.productId).price}</h4>
+                        <div className="d-flex" style={{justifyContent: "center"}}>
+                            <button type="button" className="btn btn-danger" style={{height: "30px", width: "30px"}}>-</button>
+                            <h4 style={{color: "grey"}}>x{cartItem.quantity}</h4>
+                            <button type="button" className="btn btn-success" style={{height: "30px", width: "30px"}}>+</button>
+                        </div>
+                    </div>
+                )}
+                </div>
+            </ul>
+            </div>
+          </div>
+          </div>
           </div>
         );
     }
@@ -100,6 +128,14 @@ const getCartItems = () => {
         }
     });
     return cartItems;
+}
+
+const getTotalPrice = (items) => {
+    let prices = [];
+    items.forEach((item) => {
+        prices.push(getById(item.productId).price*item.quantity);
+    });
+    return prices.reduce((prevPrice, currPrice) => prevPrice + currPrice, 0);
 }
 
 //ONLY FOR EXAMPLE:
