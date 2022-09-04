535c3ba6d054e6445360d831713ba098da15f44f
11-Yoav-Nahmani <nadav8.tch11@gmail.com>
2022-09-04 12:01:01 +0300

    fix for main page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ProductsList from "../components/ProductsList";
-import Filter from '../components/Filter';
-
+import Filter from '../components/Filter'
 import * as test from "../services/productList"
 
 
@@ -29,50 +28,14 @@ componentDidMount = () => {
 
 
 
-  filterProducts = (titleFilter, descFilter, priceMinFilter, priceMaxFilter, inStockFilter, categoryFilter) => {
-      /*
-
-      (leaving this commented because it will be filtered differently)
-
-      let filteredProducts = this.state.products.filter((product) => {
-      let titleCheck = titleFilter ? titleFilter : product.product_name;
-      let descCheck = descFilter ? descFilter : product.description;
-      let priceMinCheck = priceMinFilter ? priceMinFilter : product.price;
-      let priceMaxCheck = priceMaxFilter ? priceMaxFilter : product.price;
-      let inStockCheck = inStockFilter ? inStockFilter : product.inStock;
-      let categoryCheck = categoryFilter ? categoryFilter : product.category;
-      console.log(titleCheck);
-      console.log(descCheck);
-      console.log(priceMinCheck);
-      console.log(priceMaxCheck);
-      console.log(inStockCheck);
-      console.log(categoryCheck);
-      return ((product.product_name.toUpperCase().includes(String(titleCheck).toUpperCase()))
-      && (product.description.toUpperCase().includes(String(descCheck).toUpperCase()))
-      && (product.price >= priceMinCheck)
-      && (product.price <= priceMaxCheck)
-      && (product.inStock === inStockCheck)
-      && (product.category.toUpperCase().includes(String(categoryCheck).toUpperCase()))
-      )
-    });
-    this.setState({
-      products: filteredProducts
-    });
-    */
-  }
-
   render() {
     const { products } = this.state;
     return (
       <div className="container">
-        <aside className="set-to-side">
-          <ProductsList products={products} />
-        </div>
-        <div className="col-3 row">
-          <Filter filterProducts={this.filterProducts}/>
-        </div>
-        </aside>
+            <ProductsList products={products} />
+          <Filter />
       </div>
     );
   }
 }
+
