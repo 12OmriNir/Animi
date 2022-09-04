bce916636ecc76f4b4d31a3c9b411bd0d3a49560
07ItamarMelamed <nadav8.tch07@gmail.com>
2022-09-04 14:52:50 +0300

    Page Not Found Image

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
+import PageNotFoundImage from '../components/PageNotFoundImage'
 
-export default function PageNotFound() {
-  return (
-    <>
-        
-    </>
-  )
+export default function PageNotFound()
+{
+    return <PageNotFoundImage />
 }
