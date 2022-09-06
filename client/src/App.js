b2435733aaac4bbc595b57746a58f2b415db341a
11-Yoav-Nahmani <nadav8.tch11@gmail.com>
2022-09-06 15:26:56 +0300

    after fixing bugs

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ export default function App() {
   return (
     <div className="App">
       <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductPage />} />
