a1f82e840597bff2ffe5b36c5893ed61e4b5c333
07_Itamar_Melamed <nadav8.tch07@gmail.com>
2022-09-07 11:20:24 +0300

    Support service for heroku

--- client/src/services/productList.js
+++ client/src/services/productList.js
@@ -1,23 +1,24 @@
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://animie.herokuapp.com"
+    : "http://localhost:300d0";
 export async function getProducts(filters) {
   const qParams = new URLSearchParams({});
-  for(let key of Object.keys(filters)){
-    if(filters[key]){
-      qParams.set(key, filters[key])
+  for (let key of Object.keys(filters)) {
+    if (filters[key]) {
+      qParams.set(key, filters[key]);
     }
   }
-  
-  const res = await fetch(
-    `http://localhost:3000/api/products?${qParams}`,
-    {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    }
-  );
+
+  const res = await fetch(`${API_URL}/api/products?${qParams}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
   return await res.json();
 }
 
 export async function addProduct(task) {
-  const res = await fetch(`http://localhost:3000/api/product`, {
+  const res = await fetch(`${API_URL}/api/product`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(task),
@@ -26,7 +27,7 @@ export async function addProduct(task) {
 }
 
 export async function getProductById(id) {
-  const res = await fetch(`http://localhost:3000/api/product/${id}`, {
+  const res = await fetch(`${API_URL}/api/product/${id}`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   });
